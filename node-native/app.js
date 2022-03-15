394376cec17de6a0d2c4702cd9f88e76b2bc5b1d
Youzege <youzegehq@gmail.com>
2022-03-15 14:54:06 +0800

    feat: 路由初始化

--- node-native/app.js
+++ node-native/app.js
@@ -1,14 +1,39 @@
+import  { handleBlogRouter, handleUserRouter }  from './src/router/index.js'
+
 const serverHandle = (req, res) => {
     // 设置返回格式 JSON
     res.setHeader('Content-type', 'application/json')
 
-    const resData = {
-        name: 'youzege',
-        site: 'gis-se',
-        env: process.env.NODE_ENV
+    // 获取path
+    const url = req.url
+    req.path = url.split('?')[0]
+
+    /**
+     * 博客数据 & 路由
+     */ 
+    const blogData = handleBlogRouter(req, res)
+    if(blogData) {
+        res.end( JSON.stringify(blogData) )
+        return
     }
 
-    res.end( JSON.stringify(resData) )
+    /**
+     * 用户数据 & 路由
+     */ 
+     const userData = handleUserRouter(req, res)
+     if(userData) {
+         res.end( JSON.stringify(userData) )
+         return
+     }
+
+     /**
+      * 未命中路由，返回404
+      * text/plain 纯文本
+      */
+     res.writeHead(404, { 'Content-type': 'text/plain' })
+     res.write('404 Not Found\n')
+
+     res.end()
 }
 
-module.exports = serverHandle
+export default serverHandle
