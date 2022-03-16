d3208c5140a6f74c47ed4ab9ed477abc80806a5c
Youzege <youzegehq@gmail.com>
2022-03-17 00:53:51 +0800

    feat: 简单的加密算法

--- node-native/src/controller/user.js
+++ node-native/src/controller/user.js
@@ -1,9 +1,13 @@
 import { exec, escape } from './../db/mysql.js'
 import xss from 'xss'
+import genPassword from '../utils/cryp.js'
 
 const login = (username, password) => {
     username = escape(xss(username))
+    password = genPassword(password)
+    console.log(password)
     password = escape(xss(password))
+    
     const sql = `
         select username, realname from users where username=${username} and password=${password};
     `
